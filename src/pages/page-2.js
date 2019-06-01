63fce3363ded7ff883ff4bb2104c85ae53e7ebe6
Marv_Tron <marvinatryonjr@gmail.com>
2019-06-01 13:51:43 -0700

    probably not worth a commit butidc right now

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>This is the second page</h1>
+    <h2>Try not to fuck it up ya dick</h2>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
